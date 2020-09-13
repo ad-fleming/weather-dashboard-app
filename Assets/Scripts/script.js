84346f3d2f90b4d6e6d70d78c20a2fb2c222e128
ad-fleming <adfleming@gmail.com>
2020-09-13 13:45:06 -0400

    linked momentjs, added functionality to dynamically render forecast data to bottom of page using lat and lon of original search

--- Assets/Scripts/script.js
+++ Assets/Scripts/script.js
@@ -11,21 +11,58 @@ var searchContainerEl = $(".searchContainerEl");
 var citySearchBar = $("#citySearchBar");
 var searchCityBtn = $("#searchCityBtn");
 var dataDump = $(".temporaryDataDump");
+var cityNameEl = $(".cityDisplay");
+var temperatureEl = $(".temp");
+var humidityEl = $(".humidity");
+var windSpeedEl = $(".windSpeed");
+var uvIndexEl = $(".uvIndex");
+var day1El = $("#day1");
 // 2)Create a variable for building url query string
 // ====================== On search click ======================
 // ============================================================
 searchCityBtn.on("click", function(event){
     event.preventDefault();
     var city = citySearchBar.val().trim();
-    var currentForecastQuery = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey;
+    var currentForecastQuery = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey
     $.ajax({
         Method: "GET",
         url: currentForecastQuery,
-    }).then(function(response){
-        console.log(response);
-        dataDump.text(JSON.stringify(response));
     })
+    .then(function(response){
+        console.log(currentForecastQuery);
+        console.log(response);
+        console.log(response.coord.lat);
+        console.log(response.coord.lon);
+        var cityLat = response.coord.lat;
+        var cityLon = response.coord.lon;
+        var cityLocation = "lat=" + cityLat + "&lon=" + cityLon + "&";
+        cityNameEl.html("<h1>" + response.name + " Weather Details</h1>");
+        temperatureEl.text("Current Temp: " + response.main.temp + " °F");
+        humidityEl.text("Humidity: " + response.main.humidity + "%");
+        windSpeedEl.text("Wind Speed: " + response.wind.speed +  " MPH");
+        
+        $.ajax({
+            Method:"GET",
+            url:"https://api.openweathermap.org/data/2.5/onecall?" + cityLocation + "exclude=hourly,minutely&units=imperial&appid=" + apiKey
+        })
+        .then(function(response){
+            console.log(response);
+            for( i = 0; i < 5; i ++){
+                var forecastIcon = $("<img>");
+                forecastIcon.attr("src", "http://openweathermap.org/img/wn/" + response.daily[i].weather[0].icon + "@2x.png");
+                forecastIcon.attr("class", "forecastIcon mt-1 mb-1");
+                var forecastTemp = $('<div>');
+                forecastTemp.attr("class", "mt-1 mb-1");
+                forecastTemp.text("Temp: " + response.daily[i].temp.day +"°F");
+                var forecastHumidity =  $("<div>");
+                forecastHumidity.text("Humidity: " + response.daily[i].humidity + "%");
+                $("#" + i).append(forecastIcon,forecastTemp,forecastHumidity);
+            }
+        })
+    });
 
+  
+    
 }); //<----- This is the end of the click event 
 
 
